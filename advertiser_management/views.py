356be209fe540f68f6187ac7c457c9b3a509746d
aboots <mahdi.abootorabi2@gmail.com>
2020-10-12 22:12:54 +0330

    add home page

--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Ad, Advertiser
 
 
 def showHomePage(request):
-    return HttpResponse("hello")
-
+    advertisers_list = Advertiser.objects.all()
+    context = {"advertisers_list": advertisers_list}
+    ad_lists = Ad.objects.all()
+    for ad in ad_lists:
+        ad.incViews()
+    return render(request, 'advertiser_management/home_page.html', context)
 
 def detail(request, question_id):
     pass
